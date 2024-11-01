76f533340015e1df81d54d0095540d88b6acb345
Oscar <altepommes04@gmail.com>
2024-11-01 14:42:23 +0100

    language

--- index.php
+++ index.php
@@ -29,7 +29,7 @@ if ($conn->connect_error) {
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="de">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
